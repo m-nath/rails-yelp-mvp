855aea8aa329812996dc432183687785104b369b
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-07 15:45:04 +0900

    done

--- db/migrate/20190807021441_create_reviews.rb
+++ db/migrate/20190807021441_create_reviews.rb
@@ -2,6 +2,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.text :content
+      t.integer :rating
+      t.references :restaurant, foreign_key: true
 
       t.timestamps
     end
